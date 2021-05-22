947e427456b1593666db76be11ffbebe2399c30c
Your Name <chornak17@gmail.com>
2021-05-22 22:09:34 +0200

    lesson09

---
+++ script.js
@@ -0,0 +1,77 @@
+"use strict";
+//
+//
+const calculate = document.getElementById("start");
+console.log(calculate);
+//
+//
+const buttonFirst = document.getElementsByTagName("button")[0];
+console.log(buttonFirst);
+//
+//
+const buttonSecond = document.getElementsByTagName("button")[1];
+console.log(buttonSecond);
+//
+//
+const checkBox = document.querySelector("#deposit-check");
+console.log(checkBox);
+//
+//
+const nameIncome = document.querySelectorAll(".additional__income-item");
+console.log(nameIncome);
+//
+//
+const budgetDay = document.getElementsByClassName("result-total")[1];
+console.log(budgetDay);
+//
+//
+const expensesMonth = document.getElementsByClassName("result-total")[2];
+console.log(expensesMonth);
+//
+//
+const additionalIncome = document.getElementsByClassName("result-total")[3];
+console.log(additionalIncome);
+//
+//
+const additionalExpenses = document.getElementsByClassName("result-total")[4];
+console.log(additionalExpenses);
+//
+//
+const incomePeriod = document.getElementsByClassName("result-total")[5];
+console.log(incomePeriod);
+//
+//
+const targetMonth = document.getElementsByClassName("result-total")[6];
+console.log(targetMonth);
+//
+//
+const monthlyIncome = document.querySelector(".salary-amount");
+console.log(monthlyIncome);
+//
+//
+const additionalIncomeTitel = document.querySelector("input.income-title");
+console.log(additionalIncomeTitel);
+//
+//
+const incomeAdditional = document.querySelector(".income-amount");
+console.log(incomeAdditional);
+//
+//
+const possibleIncome = document.querySelectorAll(".additional__income-item");
+console.log(possibleIncome);
+//
+//
+const obligatoryExpenses = document.querySelector("input.expenses-title");
+console.log(obligatoryExpenses);
+//
+//
+const obligatoryExpensesSum = document.querySelector(".expenses-amount");
+console.log(obligatoryExpensesSum);
+//
+//
+const possibleCosts = document.querySelector("input.additional__expenses-item");
+console.log(possibleCosts);
+//
+//
+const calculationPeriod = document.querySelector(".period-select");
+console.log(calculationPeriod);
